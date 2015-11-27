fe8c442779c84deaf01ee1be6cb714a3ae0139b7
zhouyu <zhouyu5460@gmail.com>
2015-11-27 15:27:52 +0800

    changed yaxis scale

--- index.js
+++ index.js
@@ -318,7 +318,7 @@
         graph.Set('chart.fillstyle', ['#daf1fa', '#faa']);
         graph.Set('chart.colors', ['rgb(169, 222, 244)', 'red']);
         graph.Set('chart.linewidth', 3);
-        graph.Set('chart.ymax', 300);
+        graph.Set('chart.ymax', 10000);
         //graph.Set('chart.xmax', 100);
         graph.Set('chart.xticks', 25);
         graph.Set('chart.gutterLeft', 30);
