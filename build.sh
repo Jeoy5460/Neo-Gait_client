45cafe48fef284582e2e30c6b18dad1a225242fc
bgxo <zhouyu5460@gmail.com>
2017-05-08 18:31:49 +0800

    added checking whether android platform is added

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
-[ -d ng_android ] || cordova create ng_android
-rsync -av --progress ./* ./ng_android/www  --exclude build.sh --exclude ng_android --exclude neo_client.npp
-cd ng_android
+[ -d iot_android ] || cordova create iot_android
+rsync -av --progress ./* ./iot_android/www  --exclude build.sh --exclude iot_android --exclude neo_client.npp
+export _JAVA_OPTIONS="-Xmx1024M"
+cd iot_android
 echo "adding android"
-cordova platforms add android
+[ -d platforms/android ] || cordova platforms add android
 echo "adding plugins"
 cordova plugin add cordova-plugin-device
 cordova plugin add cordova-plugin-console
-cordova plugin add com.evothings.ble
+#cordova plugin add com.evothings.ble
+cordova plugin cordova-plugin-ble
 echo "building android"
+#cordova run android
 cordova build android
 #adb install -r platforms/android/ant-build/demoapp.apk
-adb install -r /home/zhouyu/projs/ng_box/ng_android/platforms/android/build/outputs/apk/android-debug.apk
+adb install -r /home/xFrog/projs/iotbox/iot_android/platforms/android/build/outputs/apk/android-debug.apk
